5934ad207caec8249413350ebbb22162d0aaacd2
Adithya V. Mohan <adithyavmohan@gmail.com>
2026-03-29 00:06:21 +0400

    Vercel file added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"; 
 import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Certifications from "./components/Certifications";
-// import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app-background">
       <Navbar />
       <Routes>
           <Route path="/" element={<Home />} />
@@ -18,9 +17,8 @@ function App() {
           <Route path="/Projects" element={<Projects />} />
           <Route path="/Experience" element={<Experience />} />
           <Route path="/Certifications" element={<Certifications />} />
-          {/* <Route path="/Contact" element={<Contact />} />    */}
       </Routes>
-    </>
+   </div>
   );
 }
 
